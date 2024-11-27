a944a34fb74c31c2e37f86f39064a90b875514bd
LAPTOP-AV62RQ0I\Sumanth Royal <sumanthroyal311@gmail.com>
2024-11-27 15:15:39 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../components/Home.css';
+
+
+const Home = () => {
+  return (
+    <div>
+      <div className='header'>
+        <nav>
+            <a href='/ '>Home</a>
+            <a href='/'>login</a>
+            <a href='/'>singup</a>
+            <a href='/'>contact</a>
+            
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
